f5aef7f1094fe4dcd12850d5dc0630a3525cfe24
Archimboldi <1751844336@qq.com>
2021-04-23 17:39:08 +0800

    4-23

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,23 @@ extern crate sciter;
 // use libloading::{Symbol, Library};
 // use libc::*;
 // use std::{ffi::CString, ptr::null_mut};
-// use sciter::value;
 use std::fs;
-use crossbeam_utils::thread;
-use crossbeam_queue::ArrayQueue;
+use std::thread;
+use tokio::{runtime::Runtime, sync::mpsc, net::TcpStream};
+use tokio_util::compat::TokioAsyncWriteCompatExt;
+use once_cell::sync::Lazy;
 
 struct Handler<'a>{
-    q:&'a ArrayQueue<String>,
+    tx: &'a mpsc::Sender<String>,
+    xr: &'a mut mpsc::Receiver<String>,
     dist: String
 }
 
 impl<'a> Handler<'a>{
-    fn index_sum(&self, lx:i32, sd:String, ed:String) -> String {
+    fn index_sum(&mut self, lx:i32, sd:String, ed:String) -> String {
         let msg = format!("+;{};{};{}",lx,sd,ed);
-        &self.q.push(msg);
-        let mut res = String::new();
-        loop{
-            if self.q.is_full() {
-                if let Some(c) = self.q.pop(){
-                    if c.starts_with("+") {
-                        if self.q.is_empty(){
-                            &self.q.push(c);
-                        }
-                    }else{
-                        res = c;
-                        break;
-                    }
-                };
-            }
-        }
-        res
+        &self.tx.blocking_send(msg).unwrap();
+        self.xr.blocking_recv().unwrap()
     }
     fn write(&self, d:String,e:String,f:String) -> i32 {
         0
@@ -54,100 +41,95 @@ impl<'a> sciter::EventHandler for Handler<'a> {
     }
 }
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()>{
-    use once_cell::sync::Lazy;
+fn main() {
     use tiberius::{AuthMethod, Client, Config};
-    use async_std::net::TcpStream;
-    static CONN_STR: Lazy<String> = Lazy::new(|| {
-        std::env::var("TIBERIUS_CONNECTION_STRING").unwrap_or_else(|_| {
-            "server=tcp:localhost,1433;IntegratedSecurity=true;TrustServerCertificate=true".to_owned()
-        })
+    let (tx, mut rx) = mpsc::channel::<String>(1);
+    let (xt, mut xr) = mpsc::channel::<String>(1);
+    let sync_code = thread::spawn(move || {
+        let html = include_bytes!("../ui/clock.htm");
+        // let resouces = include_bytes!("../ui.rc");
+        let mut frame = sciter::WindowBuilder::main_window().with_size((400, 300)).with_pos((540,270)).create();
+        let load = format!("file://{}/ui/clock.htm", std::env::current_dir().unwrap().to_str().unwrap());
+        frame.load_html(html, Some(&load));
+        // frame.archive_handler(resouces).expect("Invalid archive");
+        let dist: String = std::env::args().nth(1).unwrap();
+        let handler = Handler{tx:&tx, xr: &mut xr,dist:dist};
+        frame.event_handler(handler);
+        frame.load_file("this://app/clock.htm");
+        
+        frame.run_app();
+        tx.blocking_send("over".to_string()).unwrap();
     });
-    let mut config = Config::from_ado_string(&CONN_STR)?;
-    config.database("cb");
-    config.authentication(AuthMethod::sql_server("sa", "12345"));
-    let tcp = TcpStream::connect(config.get_addr()).await?;
-    tcp.set_nodelay(true)?;
-    let mut client = Client::connect(config, tcp).await?;
 
-    let q = ArrayQueue::new(1);
-    let dist: String = std::env::args().nth(1).unwrap();
-    thread::scope(|sc|{
-        sc.spawn(|_| {
-            let html = include_bytes!("../ui/clock.htm");
-            // let resouces = include_bytes!("../ui.rc");
-            let mut frame = sciter::WindowBuilder::main_window().with_size((400, 300)).with_pos((540,270)).create();
-            let load = format!("file://{}/ui/clock.htm", std::env::current_dir().unwrap().to_str().unwrap());
-            frame.load_html(html, Some(&load));
-            // frame.archive_handler(resouces).expect("Invalid archive");
-            let handler = Handler{q:&q,dist:dist};
-            frame.event_handler(handler);
-            frame.load_file("this://app/clock.htm");
-            
-            frame.run_app();
+    Runtime::new().unwrap().block_on(async move {
+        static CONN_STR: Lazy<String> = Lazy::new(|| {
+            std::env::var("TIBERIUS_CONNECTION_STRING").unwrap_or_else(|_| {
+                "server=tcp:localhost,1433;IntegratedSecurity=true;TrustServerCertificate=true".to_owned()
+            })
         });
+        let mut config = Config::from_ado_string(&CONN_STR).unwrap();
+        config.database("cb");
+        config.authentication(AuthMethod::sql_server("sa", "12345"));
+        let tcp = TcpStream::connect(config.get_addr()).await.unwrap();
+        tcp.set_nodelay(true).unwrap();
+        let mut client = Client::connect(config, tcp.compat_write()).await.unwrap();
         loop {
-            if q.is_full() {
-                if let Some(c) = q.pop(){
-                    if c.starts_with("+") {
-                        let args:Vec<&str> = c.splitn(4,";").collect();
-                        dbg!(&args);
-                        let que = format!("SELECT PID,DID,TITLE from dbo.E_FILE{} WHERE STATUS = 0 AND CREATETIME BETWEEN '{}' AND '{}'", args[1],args[2],args[3]);
-                        let stream = client
-                            .query(que,
-                                &[&1, &2, &3],
-                            )
-                            .await?;
-                        let rowsets = stream.into_results().await?;
-                        let mut ts = 0;
-                        let mut zys = 0;
-                        let mut nys = 0;
-                        let mut ys0 = 0;
-                        if let Some(rows) = rowsets.get(0) {
-                            for row in rows {
-                                ts = ts+1;
-                                let pid =row.get::<i32, _>(0).unwrap();
-                                let u = format!("SELECT YS,TITLE from dbo.D_FILE{} WHERE STATUS = 0 AND DID = {}", args[1], pid);
-                                let resu = client
-                                    .query(u, 
-                                        &[&1, &2]
-                                    )
-                                    .await?;
-                                    let res = resu.into_results().await?;
-                                    if let Some(re) = res.get(0){
-                                        for r in re {
-                                            let ys = r.get::<i32, _>(0);
-                                            if ys != None {
-                                                let s = ys.unwrap();
-                                                if s == 0 {
-                                                    ys0 = ys0+1;
-                                                }else {
-                                                    zys = zys + s;
-                                                }
-                                            }else{
-                                                nys = nys+1;
+            if let Some(c) = rx.recv().await{
+                if c.starts_with("+") {
+                    let args:Vec<&str> = c.splitn(4,";").collect();
+                    let que = format!("SELECT PID,DID,TITLE from dbo.E_FILE{} WHERE STATUS = 0 AND CREATETIME BETWEEN '{}' AND '{}'", args[1],args[2],args[3]);
+                    let stream = client
+                        .query(que,
+                            &[&1, &2, &3],
+                        )
+                        .await
+                        .unwrap();
+                    let rowsets = stream.into_results().await.unwrap();
+                    let mut ts = 0;
+                    let mut zys = 0;
+                    let mut nys = 0;
+                    let mut ys0 = 0;
+                    if let Some(rows) = rowsets.get(0) {
+                        for row in rows {
+                            ts = ts+1;
+                            let pid =row.get::<i32, _>(0).unwrap();
+                            let u = format!("SELECT YS,TITLE from dbo.D_FILE{} WHERE STATUS = 0 AND DID = {}", args[1], pid);
+                            let resu = client
+                                .query(u, 
+                                    &[&1, &2]
+                                )
+                                .await
+                                .unwrap();
+                                let res = resu.into_results().await.unwrap();
+                                if let Some(re) = res.get(0){
+                                    for r in re {
+                                        let ys = r.get::<i32, _>(0);
+                                        if ys != None {
+                                            let s = ys.unwrap();
+                                            if s == 0 {
+                                                ys0 = ys0+1;
+                                            }else {
+                                                zys = zys + s;
                                             }
+                                        }else{
+                                            nys = nys+1;
                                         }
                                     }
-                            }
-                        }
-                        let msg = format!("-{} - {}，挂接数：{}件，总页数：{}页，页数为空：{}条，页数为0：{}条", args[2],args[3],ts, zys, nys, ys0);
-                        dbg!(&msg);
-                        if q.is_empty(){
-                            q.push(msg).unwrap();
-                        }
-                    }else{
-                        if q.is_empty(){
-                            q.push(c).unwrap();
+                                }
                         }
                     }
-                };
-            }
+                    let msg = format!("-{} - {}，挂接数：{}件，总页数：{}页，页数为空：{}条，页数为0：{}条", args[2],args[3],ts, zys, nys, ys0);
+                    xt.send(msg).await.unwrap();
+                }else if c == "over".to_string() {
+                    break;
+                }
+            };
         }
-    }).unwrap();
+
+  
+    });
     
-    Ok(())
+    sync_code.join().unwrap()
 }
 
 // fn _token() -> bool {
